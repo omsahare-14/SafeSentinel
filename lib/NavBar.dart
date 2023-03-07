f1f1fda1d4864ce4b3e896f12a2ebe5ab0ba8438
devyaanshdwivedi <devyaanshdwivedi1234@gmail.com>
2023-03-07 20:25:31 +0530

    Get Location with all functionality

--- lib/NavBar.dart
+++ lib/NavBar.dart
@@ -1,9 +1,17 @@
 // TODO Implement this library.
 import 'package:flutter/material.dart';
 import 'package:getlocation/add_contacts.dart';
-//import 'package:getlocation/contacts_page.dart';
+import 'package:getlocation/contacts_page.dart';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NavBar extends StatelessWidget {
+  get latitude => null;
+
+  get longitude => null;
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -16,8 +24,8 @@ class NavBar extends StatelessWidget {
             leading: Icon(Icons.contacts),
             title: Text("Edit Emergency Contacts"),
             onTap: () => {
-              Navigator.push(
-                  context,MaterialPageRoute(builder: (context)=>AddContactsPage()))
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AddContactsPage()))
             },
           ),
           Divider(),
@@ -28,20 +36,40 @@ class NavBar extends StatelessWidget {
           ),
           Divider(),
           ListTile(
-            leading: Icon(Icons.local_police),
-            title: Text("Locate Police Station"),
-            onTap: () => null,
-          ),
-          ListTile(
-            leading: Icon(Icons.local_hospital),
-            title: Text("Locate Hospital"),
+            leading: Icon(Icons.tips_and_updates),
+            title: Text("Tip of the Day"),
             onTap: () => null,
           ),
           Divider(),
           ListTile(
+              leading: Icon(Icons.local_police),
+              title: Text("Locate Police Station"),
+              onTap: () async {
+                final Uri googleMapsUri = Uri.parse(
+                    'https://www.google.com/maps/search/?api=1&query=police+station&query=${latitude},${longitude}');
+                if (await canLaunchUrl(googleMapsUri)) {
+                  launchUrl(googleMapsUri);
+                } else {
+                  throw 'Could not launch $googleMapsUri';
+                }
+              }),
+          ListTile(
+              leading: Icon(Icons.local_hospital),
+              title: Text("Locate Hospital"),
+              onTap: () async {
+                final Uri googleMapsUri = Uri.parse(
+                    'https://www.google.com/maps/search/?api=1&query=hospital&query=${latitude},${longitude}');
+                if (await canLaunchUrl(googleMapsUri)) {
+                  launchUrl(googleMapsUri);
+                } else {
+                  throw 'Could not launch $googleMapsUri';
+                }
+              }),
+          Divider(),
+          ListTile(
             leading: Icon(Icons.exit_to_app),
             title: Text("Exit"),
-            onTap: () => null,
+            onTap: () => exit(0),
           ),
         ],
       ),
